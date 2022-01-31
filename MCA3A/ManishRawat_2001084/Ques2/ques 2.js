42424c0aaa38f6655b0ae72dffb19ceb5ab4163e
Manish Rawat <85739547+manish098rawat@users.noreply.github.com>
2022-01-31 11:29:05 +0530

    Delete MCA3A/ManishRawat_2001084/Ques2 directory

--- MCA3A/ManishRawat_2001084/Ques2/ques 2.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const server = http.createServer((req,res) =>{
-res.end("Response from server-->>" + PORT);
-});
-const PORT = process.env.PORT || 3000;
-const HOST = "localhost";
-server.listen(PORT, HOST, () =>{
-    console.log("Current server at PORT "+PORT );
-});
